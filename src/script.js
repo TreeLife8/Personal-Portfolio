9d756463bef1aaab203fb2d0e4d25d8c848663f7
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-07-30 15:20:10 +1000

    Updated details on about page, added future job card on work page

---
+++ src/script.js
@@ -0,0 +1,42 @@
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-1"),
+  {
+    container: "body",
+  }
+);
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-2"),
+  {
+    container: "body",
+  }
+);
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-3"),
+  {
+    container: "body",
+  }
+);
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-4"),
+  {
+    container: "body",
+  }
+);
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-5"),
+  {
+    container: "body",
+  }
+);
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-6"),
+  {
+    container: "body",
+  }
+);
+var popover = new bootstrap.Popover(
+  document.querySelector(".example-popover-7"),
+  {
+    container: "body",
+  }
+);
