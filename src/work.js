e96f39574b40078b6d65cbd0c58fbe3e54d4caac
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-07-31 17:37:39 +1000

    updated my service description into work page with JS

---
+++ src/work.js
@@ -0,0 +1,33 @@
+document
+  .querySelector("#my-services-one")
+  .addEventListener("mouseover", function () {
+    var txt = document.querySelector("#my-services").innerHTML;
+    document.querySelector("#my-services").innerHTML =
+      "experience excellence with ease";
+
+    this.addEventListener("mouseout", function () {
+      document.querySelector("#my-services").innerHTML = txt;
+    });
+  });
+document
+  .querySelector("#my-services-two")
+  .addEventListener("mouseover", function () {
+    var txt = document.querySelector("#my-services").innerHTML;
+    document.querySelector("#my-services").innerHTML =
+      "enjoy the journey of web development with trust";
+
+    this.addEventListener("mouseout", function () {
+      document.querySelector("#my-services").innerHTML = txt;
+    });
+  });
+document
+  .querySelector("#my-services-three")
+  .addEventListener("mouseover", function () {
+    var txt = document.querySelector("#my-services").innerHTML;
+    document.querySelector("#my-services").innerHTML =
+      "experience excellence with ease";
+
+    this.addEventListener("mouseout", function () {
+      document.querySelector("#my-services").innerHTML = txt;
+    });
+  });
